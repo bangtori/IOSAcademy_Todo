a969e58d4bdf737be1217ba61790d82382ec3dce
bangtori <58802345+bangtori@users.noreply.github.com>
2023-06-29 01:46:31 +0900

    12. done checkmarks

--- Todo_iOS Academy/ViewModels/ProfileViewViewModel.swift
+++ Todo_iOS Academy/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by 방유빈 on 2023/06/28.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel:ObservableObject{
@@ -12,6 +14,17 @@ class ProfileViewViewModel:ObservableObject{
     init(){}
     
     func toggleIsDone(item : ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("user")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
