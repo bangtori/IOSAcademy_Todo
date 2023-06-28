6eb1643ce1330d159a083bfa8cda7db0a8767cf9
bangtori <58802345+bangtori@users.noreply.github.com>
2023-06-28 20:00:30 +0900

    init project

---
+++ Todo_iOS Academy/Todo_iOS_AcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_iOS_AcademyApp.swift
+//  Todo_iOS Academy
+//
+//  Created by 방유빈 on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct Todo_iOS_AcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
