98de344b5f4e9e6d9b9c9f830f0c9ca89eebc8a0
bangtori <58802345+bangtori@users.noreply.github.com>
2023-06-29 02:38:59 +0900

    13.profile & logout

--- Todo_iOS Academy/Views/ProfileView.swift
+++ Todo_iOS Academy/Views/ProfileView.swift
@@ -12,10 +12,56 @@ struct ProfileView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                }else{
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user:User) -> some View{
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        //Info : Name, Email, Member since
+        VStack(alignment: .leading){
+            HStack{
+                Text("Name : ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack{
+                Text("Email : ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack{
+                Text("Member since : ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        //sign out
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        Spacer()
     }
 }
 
